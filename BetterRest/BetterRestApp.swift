90c2303c8b89fe3fc6894edf6cbac86f31452cd8
Rohit Patidar <patidar_rohit@yahoo.co.in>
2022-06-30 13:55:36 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rohit Patidar on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
